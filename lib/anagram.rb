00182efefd782c6ffeb8ab1f927a3735e28da18f
Joshua <joshsmayhew@gmail.com>
2022-11-25 14:04:09 +0700

    commit tree 46bff4192dce1d517af9cc9fb989c86a7961f393

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(input)
+    @word = input
+  end
+
+  def match(arr)
+    word_letters = @word.split('')
+    arr.filter { |wd| wd.split('').sort == word_letters.sort }
+  end
+end
+
+listen = Anagram.new('listen')
